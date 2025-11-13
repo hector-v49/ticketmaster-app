0552068c8d551e3a09b34db2b881bbdc4ffd4295
Hector <hectorkverduzcorayo@student.hartnell.edu>
2025-11-13 12:48:33 -0800

    Project setup

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// By Hector Verduzco, [insert name], [insert name]...
+// DUE: December 5, 2025
+// Group Project
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+    return 0;
+}
